8bd2799043e9e9a61805fe750891a9acf69c3704
root <root@ZINCPAD.localdomain>
2016-05-11 18:53:29 +0000

    setup

---
+++ main/main.ino
@@ -0,0 +1,18 @@
+#define LEFT_RELAY A4
+#define RIGHT_RELAY A3
+#define POWER_RELAY A5
+
+int state = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LEFT_RELAY, OUTPUT);
+  pinMode(RIGHT_RELAY, OUTPUT);
+  pinMode(POWER_RELAY, OUTPUT);
+  state = 0;
+}
+
+void loop() {
+
+}
